fac573d09f0a820086678da45b9b8bccab805857
Anubhav Sharma <anubhav8804602148@gmail.com>
2023-05-28 21:35:57 +0530

    22. Generate Parentheses

---
+++ CodeForces/src/medium/Solution.java
@@ -0,0 +1,50 @@
+package medium;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		System.out.println(new Solution().generateParenthesis(3));;
+	}
+	
+	public List<String> generateParenthesis(int n) {
+        Set<String> allRes = new HashSet<>();
+        for(int i=0;i<n;i++) {
+        	allRes = updateList(allRes, i);
+        }
+        return allRes.stream().toList();
+    }
+
+	private Set<String> updateList(Set<String> allRes, int n) {
+		if(n==0) {
+			allRes.add("()");
+			return allRes;
+		}
+		Set<String> updatedSet = new HashSet<>();
+		for(String curr: allRes) {
+			int up = curr.length();
+			for(int i=0;i<up;i++) {
+				if(curr.charAt(i)=='(') {
+					int open=0;
+					for(int j=i;j<up;j++) {
+						if(curr.charAt(j)=='(') open++;
+						else open--;
+						if(open==0) {
+							updatedSet.add(getUpdatedString(curr, i, j));
+						}
+					}
+				}
+			}
+			updatedSet.add("()"+curr);
+			updatedSet.add(curr+"()");
+		}
+		return updatedSet;
+	}
+
+	private String getUpdatedString(String curr, int i, int j) {
+		return curr.substring(0, i)+"("+curr.substring(i,j+1)+")"+curr.substring(j+1);
+	}
+}
---
+++ CodeForces/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Anubhav.Sharma
+ *
+ */
+module CodeForces {
+}
